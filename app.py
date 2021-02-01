4a15c46acee9e5a7fe71ff298ef1e541d73560a5
jpbonch <jp.bonchristiano@gmail.com>
2021-02-01 16:33:14 +0000

    Switched completely to Chart.JS

--- app.py
+++ app.py
@@ -1,54 +1,34 @@
 from flask import Flask, render_template
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
-from mpld3 import fig_to_html
 import urllib.request
 from datetime import date, timedelta
+from scipy.interpolate import make_interp_spline
+import numpy as np
+
 
 app = Flask(__name__)
+# ------------- add new sensor, pms5003 ####################
+longname = ["Abingdon School", "St. Blaise",
+            "Radley College", "Abingdon Prep", "Thomas Reade", "Sensor 48673"]
+shortname = ['abingdon', 'stblaise', 'radley', 'abingdonprep',
+             'thomasreade', 'otherone']  # also filename
+id = ['48281', '50299', '51969', '52053', '52049', '48673']
+colors = ['#e74697', '#0091ff', '#800000', '#a4d2c7', '#C0C0C0', '000000']
+do_plot = [True, True, True, True, True, True]
+###################################
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    today = date.today() - timedelta(days=2)
-    todaydate = today.strftime("%Y-%m-%d")
-    strngy = "<script>window.location = '/'+screen.width+'/" + \
-        todaydate + "';" + "</script>"
-    print(strngy)
-    return "<script>window.location = '/'+screen.width+'/" + todaydate + "';" + "</script>"
-
-
-@app.route("/<width>/<graphdate>", methods=['GET', 'POST'])
-def main(width, graphdate):
-    today = date.today() - timedelta(days=2)
-    start = today - timedelta(days=50)
-    date_list = []
-    for y in range(today.toordinal(), start.toordinal() - 1, -1):
-        date_list.append(date.fromordinal(y))
-
-    # ------------- add new sensor, pms5003 ####################
-    longname = ["Abingdon School", "St. Blaise",
-                "Radley College", "Abingdon Prep", "Thomas Reade", ""]
-    shortname = ['abingdon', 'stblaise', 'radley', 'abingdonprep',
-                 'thomasreade', 'otherone']  # also filename
-    id = ['48281', '50299', '51969', '52053', '52049', '48673']
-    colors = ['#e74697', '#0091ff', '#800000', '#a4d2c7', '#C0C0C0', '000000']
-    do_plot = [True, True, True, True, True, True]
-    ###################################
-
-    notfoundtext = ['' for i in range(len(id))]
+def get_graph_data(graphdate, SMOOTHNESS):
     for i in range(len(id)):
         try:
             url = "https://archive.sensor.community/" + graphdate + \
                 "/" + graphdate + "_pms5003_sensor_" + id[i] + ".csv"
             urllib.request.urlretrieve(url, shortname[i] + ".csv")
         except urllib.error.HTTPError:
-            notfoundtext[i] = 'NO DATA'
             colors[i] = '#FFFFFF'
             do_plot[i] = False
 
-    plots = [[] for i in range(len(id))]
-
+    graphdata = {"p1": [], "p2": []}
+    x_new = np.linspace(0, 24, SMOOTHNESS)
     for i in range(len(id)):
         if do_plot[i]:
             with open(shortname[i] + ".csv", 'r+') as f:
@@ -59,9 +39,8 @@ def main(width, graphdate):
             data.pop(0)
             data.pop(len(data) - 1)
 
-        # Both P1 and P2
-        for num in (6, 7):
-            if do_plot:
+            # Both P1 and P2
+            for num in (6, 7):
                 xs = []
                 ys = []
                 for row in data:
@@ -73,27 +52,36 @@ def main(width, graphdate):
                     splitted = x.split(':')
                     xs[xs.index(x)] = float(splitted[0]) + \
                         float(splitted[1]) / 60 + float(splitted[2]) / 3600
-            else:
-                xs = list(range(10))
-                ys = list(range(10))
 
-            fig = Figure(figsize=(int(width) * (0.00484375 / 1.42),
-                                  int(width) * 0.5625 * 0.00625 / 1.3))
-            axis = fig.add_subplot(1, 1, 1)
-            axis.set_xlabel('Time')
-            axis.set_ylabel('µg/m³')
-            axis.text(((max(xs) - min(xs)) / 2) + min(xs), max(ys) / 2,
-                      notfoundtext[i], size=18, ha='center', va='center', color='#A0A0A0')
-            toprighttext = 'PM10' if num == 6 else 'PM2.5'
-            if notfoundtext[i] == '':
-                axis.text(((max(xs) - min(xs)) / 1.1) + min(xs), max(ys) / 1.05,
-                          toprighttext, size=30, ha='center', va='center', color='#A0A0A0')
+                a_BSpline = make_interp_spline(xs, ys)
 
-            axis.plot(xs, ys, c=colors[i])
-            plots[i].append(fig_to_html(fig))
+                y_new = a_BSpline(x_new)
 
-    print(plots)
-    return render_template('index.html', plots=plots, date_list=date_list, id=id, longname=longname)
+                y_new = [abs(ele) for ele in y_new]
+                if num == 6:
+                    graphdata["p1"].append(y_new)
+                else:
+                    graphdata["p2"].append(y_new)
+            # handle no data TODO
+            #fix 2020-01-14
+
+    return graphdata, x_new
+
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/<graphdate>", methods=['GET', 'POST'])
+def main(graphdate=None):
+    today = date.today() - timedelta(days=2)
+    start = today - timedelta(days=100)
+    date_list = []
+    for y in range(today.toordinal(), start.toordinal() - 1, -1):
+        date_list.append(date.fromordinal(y))
+    if graphdate is None:
+        graphdate = str(today)
+
+    graphdata, x_new = get_graph_data(graphdate, 300)
+
+    return render_template('index.html', graphdata=graphdata, date_list=date_list, longname=longname, x_new=x_new, colors=colors)
 
 
 if __name__ == "__main__":
