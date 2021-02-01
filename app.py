00f87b90b7a7fd8acc8d1317d26df0522f263c93
jpbonch <jp.bonchristiano@gmail.com>
2021-02-01 21:15:00 +0000

    Repositioning smooth plot slider

--- app.py
+++ app.py
@@ -6,98 +6,103 @@ import numpy as np
 
 
 app = Flask(__name__)
-# ------------- add new sensor, pms5003 ####################
-longname = ["Abingdon School", "St. Blaise",
-            "Radley College", "Abingdon Prep", "Thomas Reade", "Sensor 48673"]
-shortname = ['abingdon', 'stblaise', 'radley', 'abingdonprep',
-             'thomasreade', 'otherone']  # also filename
-id = ['48281', '50299', '51969', '52053', '52049', '48673']
-colors = ['#e74697', '#0091ff', '#650000', '#a4d2c7', '#C0C0C0', '#000000']
-do_plot = [True, True, True, True, True, True]
-###################################
+
+
 
 
 def get_graph_data(graphdate, smoothness):
-    for i in range(len(id)):
+    to_remove = []
+    for i in range(len(info["id"])):
         try:
             url = "https://archive.sensor.community/" + graphdate + \
-                "/" + graphdate + "_pms5003_sensor_" + id[i] + ".csv"
-            urllib.request.urlretrieve(url, shortname[i] + ".csv")
+                "/" + graphdate + "_pms5003_sensor_" + info['id'][i] + ".csv"
+            urllib.request.urlretrieve(url, info["shortname"][i] + ".csv")
         except urllib.error.HTTPError:
-            print("Error downloading " + shortname[i])
-            colors[i] = '#FFFFFF'
-            do_plot[i] = False
+            print("Error downloading " + info["shortname"][i])
+            to_remove.append(i)
+
+    deleted = []
+    for index in to_remove:
+        deleted.append(info["longname"][index])
+        for k in info.keys():
+            info[k].pop(index)
 
     graphdata = {"p1": [], "p2": []}
 
     if smoothness == "smooth":
-        x_new = np.linspace(0, 24, 24)
+        x_new = list(range(0,24))
     else:
         x_new = np.linspace(0, 24, 300)
 
-    for i in range(len(id)):
-        if do_plot[i]:
-            with open(shortname[i] + ".csv", 'r+') as f:
-                data = f.read()
-                data = data.split('\n')
-            for x in range(len(data)):
-                data[x] = data[x].split(';')
-            data.pop(0)
-            data.pop(len(data) - 1)
+    for i in range(len(info["id"])):
+        with open(info["shortname"][i] + ".csv", 'r+') as f:
+            data = f.read()
+            data = data.split('\n')
+        for x in range(len(data)):
+            data[x] = data[x].split(';')
+        data.pop(0)
+        data.pop(len(data) - 1)
 
-            # Both P1 and P2
-            for num in (6, 7):
-                xs = []
-                ys = []
-                for row in data:
-                    xs.append(row[5])
-                    ys.append(int(row[num]))
+        # Both P1 and P2
+        for num in (6, 7):
+            xs = []
+            ys = []
+            for row in data:
+                xs.append(row[5])
+                ys.append(int(row[num]))
 
-                xs = [x[11:] for x in xs]
-                for x in xs:
-                    splitted = x.split(':')
-                    xs[xs.index(x)] = float(splitted[0]) + \
-                        float(splitted[1]) / 60 + float(splitted[2]) / 3600
+            xs = [x[11:] for x in xs]
+            for x in xs:
+                splitted = x.split(':')
+                xs[xs.index(x)] = float(splitted[0]) + \
+                    float(splitted[1]) / 60 + float(splitted[2]) / 3600
 
-                a_BSpline = make_interp_spline(xs, ys)
+            a_BSpline = make_interp_spline(xs, ys)
 
-                y_new = a_BSpline(x_new)
+            y_new = a_BSpline(x_new)
 
-                y_new = [abs(ele) for ele in y_new]
-                if num == 6:
-                    graphdata["p1"].append(y_new)
-                else:
-                    graphdata["p2"].append(y_new)
-            # handle no data TODO
-            #fix 2020-01-14
-            # 25th
-            # fix places not showing up
-            # hmtl file loops thrpugh deleted places
+            y_new = [abs(ele) for ele in y_new]
+            if num == 6:
+                graphdata["p1"].append(y_new)
+            else:
+                graphdata["p2"].append(y_new)
+        # fix 2020-01-14
+        # 25th
 
     print(graphdata)
-    return graphdata, x_new
+    return graphdata, x_new, deleted
 
 
-@app.route("/", methods=['GET', 'POST'])
+@ app.route("/", methods=['GET', 'POST'])
 def index():
     today = date.today() - timedelta(days=2)
     return "<script>window.location = '/" + str(today) + "/smooth';</script>" + '\n' + (
     """<link rel="shortcut icon" href="{{ url_for('static', filename='favicon.ico') }};">""")
 
 
-@app.route("/<graphdate>/<smoothness>", methods=['GET', 'POST'])
-def main(graphdate=None, smoothness='smooth'):
-    today = date.today() - timedelta(days=2)
-    start = today - timedelta(days=100)
-    date_list = []
+@ app.route("/<graphdate>/<smoothness>", methods = ['GET', 'POST'])
+def main(graphdate = None, smoothness = 'smooth'):
+
+    global info
+    info = {
+    "longname": ["Abingdon School", "St. Blaise", "Radley College", "Abingdon Prep", "Thomas Reade", "Sensor 48673"],
+    "shortname": ['abingdon', 'stblaise', 'radley', 'abingdonprep', 'thomasreade', 'otherone'],
+    "id": ['48281', '50299', '51969', '52053', '52049', '48673'],
+    "colors": ['#e74697', '#0091ff', '#650000', '#a4d2c7', '#C0C0C0', '#000000']
+    }
+
+
+    today=date.today() - timedelta(days = 2)
+    start=today - timedelta(days = 100)
+    date_list=[]
     for y in range(today.toordinal(), start.toordinal() - 1, -1):
         date_list.append(date.fromordinal(y))
     if graphdate is None:
-        graphdate = str(today)
+        graphdate=str(today)
 
-    graphdata, x_new = get_graph_data(graphdate, smoothness)
+    graphdata, x_new, deleted = get_graph_data(graphdate, smoothness)
 
-    return render_template('index.html', graphdata=graphdata, date_list=date_list, longname=longname, x_new=x_new, colors=colors)
+    return render_template('index.html', graphdata = graphdata, date_list = date_list, longname = info["longname"], x_new = x_new, colors = info["colors"], deleted=deleted)
 
 
 if __name__ == "__main__":
